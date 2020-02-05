7bbf2cd13d8534a2024060e4205b69eba8033433
Korben Rusek <korben.rusek@gmail.com>
2020-02-05 07:21:18 -0800

    initial commit

---
+++ FastNeuralNetwork/FastNeuralNetwork/FastNeuralNetwork.h
@@ -0,0 +1,19 @@
+//
+//  FastNeuralNetwork.h
+//  FastNeuralNetwork
+//
+//  Created by Korben Rusek on 1/28/20.
+//  Copyright © 2020 Korben Rusek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FastNeuralNetwork.
+FOUNDATION_EXPORT double FastNeuralNetworkVersionNumber;
+
+//! Project version string for FastNeuralNetwork.
+FOUNDATION_EXPORT const unsigned char FastNeuralNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastNeuralNetwork/PublicHeader.h>
+
+
